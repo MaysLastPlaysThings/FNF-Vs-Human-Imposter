a3c74c8b1cf376cad7a92e896ab3e3c8a87e5dea
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-07-01 21:26:24 +0300

    took somewhere

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -85,6 +85,7 @@ class CreditsState extends MusicBeatState
 			['MaysLastPlay',		'MaysLastPlay',		'le Trollge',							'https://www.youtube.com/channel/UCx0LxtFR8ROd9sFAq-UxDfw',	'5DE7FF'],
       ['MarioMaster',		    'mariomaster',		    'hola de nuevo',	 'https://www.youtube.com/c/MarioMaster1997',	'D10616'],
        ['FNF BR',		    'fnf-br',		    '',	 'https://youtube.com/@FNF-BR',	'3E813A'],
+			['Ralsei',		'ralsei',		'Fluffy Smol',						'https://youtube.com/@ralsei.smol03',	'4dcc8e'],
 			[''],
       ['Impostars Team'],
       ['The Gamebanana credit', 'Check The Gamebanana credit', 'https://gamebanana.com/mods/448240', 'CF2D2D'],
